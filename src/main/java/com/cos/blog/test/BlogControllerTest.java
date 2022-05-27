53b7683ab98360ac31ab8978e056ced6df51c768
gihye7 <user@email.mail>
2022-05-27 15:43:00 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,17 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller 특정 어노테이션이 붙어있는 클래스 파일들을 new 해서(Ioc) 스프링 컨테이너에 관리해줍니다. 
+
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>Hello Spring boot</h1>";
+	}
+
+}
